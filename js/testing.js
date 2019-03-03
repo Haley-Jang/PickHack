801b4496694f681fc9a4a23bca1c8d2b9f2c1a7b
michael <mamichael326@gmail.com>
2019-03-03 03:53:58 -0500

    were going

--- js/testing.js
+++ js/testing.js
@@ -1,17 +1,35 @@
 
-function work () {
-    var thing = document.getElementById("penis");
-    var search = document.getElementById("searchbox").value;
-const http = new XMLHttpRequest();
-const url = "https://api.nal.usda.gov/ndb/search/?format=json&q="+search+"&max=10&offset=0&api_key=0VFfh2njUUNOAIkGcRzmgUOkbLDUKcRtP8t1ZSjY";
-http.open("GET", url);
-http.send();
-var jsonResponse;
-http.onreadystatechange=(e)=>{
-    thing.innerHTML = http.responseText;
+function work() {
+  var search = document.getElementById("searchbox").value;
+  const http = new XMLHttpRequest();
+  const url = "https://api.nal.usda.gov/ndb/search/?format=json&q=" + search + "&max=4&offset=0&api_key=0VFfh2njUUNOAIkGcRzmgUOkbLDUKcRtP8t1ZSjY";
+  http.open("GET", url);
+  http.send();
+  var jsonResponse;
+  http.onreadystatechange = (e) => {
     jsonResponse = JSON.parse(http.responseText);
-    thing.innerHTML = jsonResponse.list.item[0].name;
-}
+    var ndbnos = [" ", " ", " ", " "];
+    for (var p = 0; p < 4; p++) {
+      ndbnos[p] = jsonResponse.list.item[p].ndbno;
+    }
+    var table = document.getElementById("mytab1");
+    for (var i = 1, row; row = table.rows[i]; i++) {
+      //iterate through rows
+      //rows would be accessed using the "row" variable assigned in the for loop
+      var url1 = "https://api.nal.usda.gov/ndb/reports/?ndbno=" + ndbnos[i] + "&type=b&format=json&api_key=0VFfh2njUUNOAIkGcRzmgUOkbLDUKcRtP8t1ZSjY";
+        var http1 = new XMLHttpRequest();
+        console.log(ndbnos);
+        http1.open("GET", url1);
+        http1.send();
+        var jsonResponse1;
+        http1.onreadystatechange = (e) => {
+          jsonResponse1 = JSON.parse(http1.responseText);
+          console.log(jsonResponse1);
+        }
+      for (var j = 0, col; col = row.cells[j]; j++) {
+        console.log(jsonResponse1.report.food.name);
+      }
+    }
 
   }
   function writeUserData(userId, name, email) {
@@ -21,3 +39,4 @@ http.onreadystatechange=(e)=>{
       email: email,
     });
   }
+}
